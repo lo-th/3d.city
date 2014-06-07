bf6484360a2ce7b1d3108657dfd212185c2d25ae
lo-th <3dflashlo@gmail.com>
2014-06-07 19:58:44 +0200

    reduction

--- _compile/build.bat
+++ _compile/build.bat
@@ -1,2 +1,5 @@
 python build.py --include city --output ../build/city.js
 python build.py --include city --minify --output ../build/city.min.js
+
+python build.py --include city.3d --output ../build/city.3d.js
+python build.py --include city.3d --minify --output ../build/city.3d.min.js
